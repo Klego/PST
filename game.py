2c1f79df6e104945b37266fe3343888adbdce146
Klego <justomcollado@gmail.com>
2020-11-03 13:26:36 -0800

    Started function game_init() and fininished ability resurrect()

--- game.py
+++ game.py
@@ -1,13 +1,14 @@
 from characters import *
 from enemies import *
-class Game:
+
+def game_init():
+    turns = 0
+    characters_playing = []
 
 
-
-
-    def resurrect(character):
-        if character.get_alive() == True:
-            return 0
-        else:
-            character.set_alive()
-            character.set_max_health()
+def resurrect(character):
+    if character.get_alive():
+        return 0
+    else:
+        character.set_alive()
+        character.set_max_health()
