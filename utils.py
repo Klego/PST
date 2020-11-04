cf5b44dab7c62e6e2cc96d6b570e9edf407d2419
Klego <justomcollado@gmail.com>
2020-11-04 14:00:51 -0800

    added parse_arguments function

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import sys
-
+import argparse
+NUM_MAX_STAGES = 10
+NUM_MAX_PLAYERS = 10
 
 class InvalidInputError(Exception):
     pass
@@ -32,3 +34,32 @@ def sanitize_inputs(message="", valid_input=[], valid_cast=str, num_retries=-1):
 
 def clear_screen():
     os.system('clear')
+
+def arguments_parser():
+    try:
+        exit = False
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-p', '--players', type=int, default=1)
+        parser.add_argument('-s', '--stages', type=int, default=1)
+        args = parser.parse_args()
+        if not 0 < args.players <= 4:
+            print("The number of players must be between 1 and 4. Finishing program")
+            exit = True
+        else:
+            num_players = args.players
+        if not 0 < args.stages <= 10:
+            print("The number of stages must be between 1 and 10. Finishing program")
+            exit = True
+        else:
+            num_stages = args.stages
+        if exit:
+            sys.exit(0)
+        else:
+            return num_players, num_stages
+    except ValueError:
+        print("You must input only integers. Finishing program")
+
+
+
+
+
